849995b11ad01222a44cb274bda04bd11ea74938
Andrii-Cherevatov <alwayswannafly2121@gmail.com>
2023-04-18 13:59:50 +0300

    Created Nearest Arrivals Service

--- Services/StationService/StationService.cs
+++ Services/StationService/StationService.cs
@@ -52,5 +52,34 @@ namespace TransportSystem.Services.StationService
             response.Message = "No timetable for this station";
             return response;
         }
+
+        public async Task<ServiceResponse<List<StationTimetableDto>>> GetNearestArrivals(int stationId)
+        {
+            var response = new ServiceResponse<List<StationTimetableDto>>();
+            var timeNow = DateTime.Now.TimeOfDay;
+
+            response.Data = await _context.Timetables 
+                .Where(t => 
+                        t.StationId == stationId 
+                            && t.ArrivalTime > timeNow
+                                //Here is the comparison of current time and arrival time. 60 stands for minutes (1 hour) 
+                                && EF.Functions.DateDiffMinute(timeNow, t.ArrivalTime) < 60)  
+                .OrderBy(x => x.ArrivalTime)
+                .Select(dto => new StationTimetableDto()
+                {
+                    ArrivalTime = dto.ArrivalTime - timeNow,
+                    TransportUnitId = dto.TransportUnitId
+                }).ToListAsync();
+
+            if (response.Data.Count > 0)
+            {
+                response.Success = true;
+                return response;
+            }
+
+            response.Success = false;
+            response.Message = "Oops, no arrivals in one hour...";
+            return response;
+        }
     }
 }
