f6ca30c9689b93119c94b5e9598251707d30fc4a
Andrii-Cherevatov <alwayswannafly2121@gmail.com>
2023-05-08 04:20:11 +0300

    SignIn/SignUp implemented

--- reactclient/src/App.js
+++ reactclient/src/App.js
@@ -4,6 +4,8 @@ import RideHistory from './pages/RideHistory'
 import StationTimetable from './pages/StationTimetable';
 import WelcomePage from './pages/WelcomePage'
 import UserTicketHistory from './pages/UserTicketHistory';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 
 function App() {
@@ -16,6 +18,8 @@ function App() {
                 <Route path="/RideHistory" element={<RideHistory />} />
                 <Route path="/StationTimetable" element={<StationTimetable />} /> 
                 <Route path="/UserTicketHistory" element={<UserTicketHistory />} /> 
+                <Route path="/SignIn" element={<SignIn />} /> 
+                <Route path="/SignUp" element={<SignUp />} /> 
             </Routes>
         </BrowserRouter>
     </>
